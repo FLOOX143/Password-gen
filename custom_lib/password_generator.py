336db729179b7daff5357dd5583dacee7889be95
Dmitry <110310049+FLOOX143@users.noreply.github.com>
2023-11-30 23:52:58 +0300

    Update password_generator.py

--- custom_lib/password_generator.py
+++ custom_lib/password_generator.py
@@ -11,21 +11,16 @@ def passgen(x):
                 'k', 'l', 'm', 'n', 'o',
                 'p', 'q', 'r', 's', 't',
                 'u', 'v', 'w', 'x', 'y', 'z']
-<<<<<<< HEAD
-=======
     special_syms = ['/', '_', '!', '%', '*', '(', ')', '#', '@', '&', ':']
->>>>>>> 19676a8 (db hotfix, pass gen rabased)
+
 
     for i in range(x):
         password_p1.append(random.choice(alphabet))
         password_p2.append(random.randint(1, 9))
-<<<<<<< HEAD
         random.shuffle(alphabet)
-=======
         password_p1.append(random.choice(special_syms))
         random.shuffle(alphabet)
         random.shuffle(special_syms)
->>>>>>> 19676a8 (db hotfix, pass gen rabased)
 
     password.extend(password_p1)
     password.extend(password_p2)
